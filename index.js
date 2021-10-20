eb501fcbe92f1b8f0573d8c5686fe4d2ad4e9693
rsneal01 <rsneal01@gmail.com>
2021-10-20 09:28:22 -0700

    commit tree 8473f8165575a7311593fa782618e35763eea552

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your functions here
+
+function map(sourceArray, callback){
+    const a = [];
+    for (let i=0; i<sourceArray.length; i++){
+        // const number = sourceArray[i]
+        a.push(callback(sourceArray[i]));
+
+    }
+    return a;
+    // return map(sourceArray, function(a){ return a * -1 })
+}
+
+function reduce(sourceArray, callback, startingPoint=0){
+    let total
+    if (startingPoint){
+        total = startingPoint;
+        for (let i=0; i<sourceArray.length; i++){
+            // const number = sourceArray[i]
+            total = callback(sourceArray[i], total);
+        }
+        return total;
+    }
+    
+    else {
+        total = sourceArray[0];
+        for (let i=1; i<sourceArray.length; i++){
+            // const number = sourceArray[i]
+            total = callback(sourceArray[i], total);
+        }
+        return total;
+    }
+}
+    
+  
